50647dda7d95a40284e566dbbbd05b5880bfa884
shrutimannikeri <33412806+shrutimannikeri@users.noreply.github.com>
2022-07-15 14:45:59 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -62,105 +62,13 @@ let simple_resume={
       }
     ]
   }
-  console.log(simple_resume)
 
-  {
-    personal: {
-      name: 'shruti',
-      label: 'Software Engineer',
-      email: 'shrumanniker@gmail.com',
-      phone: '8951211056',
-      location: {
-        address: '#105 6th cross agara village hsr',
-        postalCode: '560102',
-        city: 'Bengalore',
-        state: 'KA',
-        countryCode: 'IN'
-      },
-      experience: '4 year',
-      salary: '9.3 lpa',
-      profiles: [ [Object] ]
-    },
-    work: [
-      {
-        company_name: 'Gartner',
-        position: 'Software Enginner',
-        url: 'https://www.gartner.com/en'
-      }
-    ],
-    education: [
-      {
-        institution: 'TCE Enginner college',
-        area: 'Computer Science',
-        studyType: 'BE',
-        startDate: '2013-08-04',
-        endDate: '2017-04-020',
-        score: '4.0'
-      }
-    ],
-    skills: [ { name: 'Web Development', level: 'Master', keywords: [Array] } ],
-    languages: [
-      { language: 'English', fluency: 'advance' },
-      { language: 'Kannada', fluency: 'Native speaker' }
-    ]
-  }
-  [
-    {
-      personal: {
-        name: 'shruti',
-        label: 'Software Engineer',
-        email: 'shrumanniker@gmail.com',
-        phone: '8951211056',
-        location: [Object],
-        experience: '4 year',
-        salary: '9.3 lpa',
-        profiles: [Array]
-      },
-      work: [ [Object] ],
-      education: [ [Object] ],
-      skills: [ [Object] ],
-      languages: [ [Object], [Object] ]
-    }
-  ]
+  console.log(simple_resume)
   
-//   C:\Users\smannike\Desktop\guvi_class>node resume.js
-//   {
-//     personal: {
-//       name: 'shruti',
-//       label: 'Software Engineer',
-//       email: 'shrumanniker@gmail.com',
-//       phone: '8951211056',
-//       location: {
-//         address: '#105 6th cross agara village hsr',
-//         postalCode: '560102',
-//         city: 'Bengalore',
-//         state: 'KA',
-//         countryCode: 'IN'
-//       },
-//       experience: '4 year',
-//       salary: '9.3 lpa',
-//       profiles: [ [Object] ]
-//     },
-//     work: [
-//       {
-//         company_name: 'Gartner',
-//         position: 'Software Enginner',
-//         url: 'https://www.gartner.com/en'
-//       }
-//     ],
-//     education: [
-//       {
-//         institution: 'TCE Enginner college',
-//         area: 'Computer Science',
-//         studyType: 'BE',
-//         startDate: '2013-08-04',
-//         endDate: '2017-04-020',
-//         score: '4.0'
-//       }
-//     ],
-//     skills: [ { name: 'Web Development', level: 'Master', keywords: [Array] } ],
-//     languages: [
-//       { language: 'English', fluency: 'advance' },
-//       { language: 'Kannada', fluency: 'Native speaker' }
-//     ]
-//   }
+// {personal: {…}, work: Array(1), education: Array(1), skills: Array(1), languages: Array(2)}
+// education: [{…}]
+// languages: (2) [{…}, {…}]
+// personal: {name: 'shruti', label: 'Software Engineer', email: 'shrumanniker@gmail.com', phone: '8951211056', location: {…}, …}
+// skills: [{…}]
+// work: [{…}]
+// [[Prototype]]: Object
